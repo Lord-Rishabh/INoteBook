22d9ee918908bb84371a1234b23fee617c13d6f5
Rishabh <rishabhptl2003@gmail.com>
2023-09-02 10:44:33 +0530

    Finalizing application

--- inotebook/src/components/About.js
+++ inotebook/src/components/About.js
@@ -1,6 +1,6 @@
 // import { useContext, useEffect } from 'react'
 // import NoteContext from '../context/noteContent'
-
+import '../index.css';
 const About = () => {
 //   const a = useContext(NoteContext);
 //   useEffect(() => {
@@ -10,7 +10,15 @@ const About = () => {
 
   return (
     <>
-    <p> This is about </p>
+    <section class="about">
+            <h2>Hello, I'm Rishabh Patel</h2>
+            <p>Welcome to INoteBook, a platform for creating, updating, and deleting notes. I'm passionate about helping you organize your thoughts and tasks efficiently.</p>
+            <p>About the developer : I'm a dedicated developer with a knack for building web applications using the MERN (MongoDB, Express.js, React, Node.js) stack. I thrive on creating efficient and user-friendly software that solves real-world problems.
+
+My journey in software development started with a curiosity for technology and a passion for coding. Over the years, I've honed my skills in web development, and I'm always excited to learn and explore new technologies.</p>
+            <p>Connect with me on LinkedIn :&nbsp; 
+            <a href="https://www.linkedin.com/in/rishabh-patel01/">Linkedin Profile</a></p>
+        </section>
     </>
   )
 }
--- inotebook/src/components/Navbar.js
+++ inotebook/src/components/Navbar.js
@@ -24,7 +24,7 @@ const Navbar = (props) => {
     <div>
       <nav className="navbar navbar-expand-lg fixed-top navbar-dark bg-dark">
         <div className="container-fluid">
-          <Link className="navbar-brand" to="/">Navbar</Link>
+          <Link className="navbar-brand" to="/"><i class="fa-solid fa-book"></i>&nbsp;INoteBook</Link>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
